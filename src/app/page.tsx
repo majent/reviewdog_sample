5f4e1fa3cc0af4689272803c1af80092510ae544
Ryo Omori <ryo763783@gmail.com>
2024-12-10 15:26:55 +0900

    calc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { sum } from "@/utils/sum";
 import Image from "next/image";
 import { JSX } from "react";
 
 export default function Home(): JSX.Element {
   const num = 1;
   const str = "Hello, ReviewDog!";
-
-  console.log(sum(1, 2));
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
