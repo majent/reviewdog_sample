9c74b740a85dc797cd0efa7cd4d2bd208ff261a0
Ryo Omori <ryo763783@gmail.com>
2024-12-10 20:15:40 +0900

    resolver

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,7 +1,29 @@
-import fleet from "@/schema/query/fleet";
+type Args = {
+  id: string;
+  organizationId: string;
+};
+
+type Fleet = {
+  id: string;
+  organizationId: string;
+  name: string;
+};
+
+type Context = {
+  organizationId?: string;
+};
 
 export const resolvers = {
   Query: {
-    fleet,
+    getFleet: async (
+      _: unknown,
+      { id, organizationId }: Args,
+      { organizationId: contextOrganizationId }: Context
+    ): Promise<Fleet> => {
+      if (!contextOrganizationId) {
+        throw new Error("Unauthorized");
+      }
+      return { id, name: "羽田空港送迎", organizationId: organizationId };
+    },
   },
 };
