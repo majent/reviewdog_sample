36ceaaaa59b893ae9f464fee9897f8815b8301b6
Ryo Omori <ryo763783@gmail.com>
2024-12-10 11:33:25 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home(): JSX.Element {
   const str = "Hello, ReviewDog!";
 
   console.log(sum(1, 2));
+  console.log(sum(1, 2));
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
